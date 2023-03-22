9e727d56a0c7e9db7c2e8d633bec38bc0804f456
root <root@590883b65072>
2023-03-22 01:20:39 +0000

    OBedient Robot

--- src/_03_method_writing/_1_obedient_robot/Obedient_R0bot.java
+++ src/_03_method_writing/_1_obedient_robot/Obedient_R0bot.java
@@ -1,28 +1,51 @@
 package _03_method_writing._1_obedient_robot;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Obedient_R0bot {
+	public static Robot rob = new Robot();
 
-	
-		Robot rob = new Robot();
-	
-		
-	
+	public static void main(String args[]) {
+
 		rob.penDown();
-		rob.setPenColor(0,0,0);
-	rob.show();
-	rob.move(90);
-	rob.turn(90);
-	rob.move(90);
-	rob.turn(90);
-	rob.move(90);
-	rob.turn(90);
-	rob.move(90);
+		rob.setSpeed(100);
+		rob.setPenColor(0, 0, 0);
+		rob.show();
+		drawSquare();
+		drawTriangle();
+		drawCircle();
 	}
 
-	
+	public static void drawSquare() {
+		for (int i = 0; i < 4; i++) {
+			rob.move(90);
+			rob.turn(90);
+		}
 	}
-	
 
+	public static void drawTriangle() {
+		for (int i = 0; i < 3; i++) {
+			rob.move(90);
+			rob.turn(120);
+		}
+	}
 
+	public static void drawCircle() {
+		for (int i = 0; i < 360; i++) {
+			rob.move(1);
+			rob.turn(1);
+		}
+	}
+
+	String shape = JOptionPane.showInputDialog("What shape do you want?");
+	{
+		if (shape.equals("Square")) {
+for(int i = 0; i < 4; i++) {
+	rob.move(90);
+	rob.turn(90);
+}
+		}
+	}
+}
