58449b1d3a7a244434b9ad078e19d7984a748273
root <root@bb727169a341>
2023-04-12 01:20:17 +0000

    Practice1,Calculator

--- src/_03_method_writing/_4_calculator/Calculator.java
+++ src/_03_method_writing/_4_calculator/Calculator.java
@@ -14,18 +14,53 @@ public class Calculator {
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" }, null);
 
 		// 3) Call the correct method depending on what option the user chooses
+		int result = 0;
+		if (task == 3) {
+			result = add(number1,number2);
+		}
+		int result1 = 0;
+		if (task == 2) {
+			result = subtract(number1,number2);
+		}
 		
+		int result2 = 0;
+		if (task == 1) {
+			result = multiply(number1,number2);
+		}
+		int result3 = 0;
+		if (task == 0) {
+			result = divide(number1,number2);
+		}
+			
+		int result11 = subtract(number1,number2);
+		int result21 = multiply(number1,number2);
+		int result31 = divide(number1,number2);
 		// 4) Call the result() method and put the answer in a pop-up
+	System.out.print(result(result));
 	}
+	
 	
 	// 1) Make 4 static methods (add, subtract, multiply, divide)
 	//		They should each take 2 numbers as parameters
 	//		and return the answer.
-	 
+	 public static int add(int a, int b) {
+		return a+b;
+	 }
+	 public static int subtract(int a, int b) {
+		 return a-b;
+	 }
+	 public static int multiply(int a, int b) {
+		 return a * b;
+	 }
+	 public static int divide(int a, int b) {
+		 return a / b;
+	 }
 	
 	// 2) Make a static method called " result" that takes a number
 	//		as a parameter and returns the number in a string
 	//		EX: "Your answer is " + number;
 
-	
+	public static String result(int r) {
+		return "Your answer is" + r;
+	}
 }
---
+++ src/_04_practice/_1_skill_practice/Practice1.java
@@ -0,0 +1,7 @@
+package _04_practice._1_skill_practice;
+
+public class Practice1 {
+public static void main(String args[]) {
+	
+}
+}
